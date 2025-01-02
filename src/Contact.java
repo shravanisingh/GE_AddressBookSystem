0b9ccc26b4f4e83990b34781a16b3654ee09238e
shravnisingh <shravanisingh22@gmail.com>
2025-01-03 01:08:19 +0530

    UC-11

--- src/Contact.java
+++ src/Contact.java
@@ -1,4 +1,3 @@
-import java.util.Objects;
 public class Contact {
     String firstName;
     String lastName;
@@ -7,6 +6,7 @@ public class Contact {
     String email;
     int phone;
     int zip;
+
     public Contact(String firstName, String lastName, String city, String state, String email, int phone, int zip) {
         this.firstName = firstName;
         this.lastName = lastName;
@@ -16,27 +16,10 @@ public class Contact {
         this.phone = phone;
         this.zip = zip;
     }
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Contact contact = (Contact) o;
-        return firstName.equalsIgnoreCase(contact.firstName) && lastName.equalsIgnoreCase(contact.lastName);
-    }
-    @Override
-    public int hashCode() {
-        return Objects.hash(firstName.toLowerCase(), lastName.toLowerCase());
-    }
+
     @Override
     public String toString() {
-        return "Contact{" +
-                "firstName='" + firstName + '\'' +
-                ", lastName='" + lastName + '\'' +
-                ", city='" + city + '\'' +
-                ", state='" + state + '\'' +
-                ", email='" + email + '\'' +
-                ", phone=" + phone +
-                ", zip=" + zip +
-                '}';
+        return String.format("Name: %s %s, City: %s, State: %s, Email: %s, Phone: %d, ZIP: %d",
+                firstName, lastName, city, state, email, phone, zip);
     }
 }
